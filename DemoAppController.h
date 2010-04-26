dd9dc875426bf231eeb6e0469bd3049c0d67bbc7
Robert Payne <robertpayne@me.com>
2010-04-26 12:59:14 +1200

    Demo App Added

---
+++ DemoAppController.h
@@ -0,0 +1,17 @@
+//
+//  PSMTabBarControlDemoAppDelegate.h
+//  PSMTabBarControl
+//
+//  Created by Robert Payne on 4/26/10.
+//  Copyright 2010 Zwopple. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface DemoAppController : NSObject <NSApplicationDelegate> {
+}
+
+- (IBAction)newWindow:(id)pSender;
+
+@end
